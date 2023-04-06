3c5b24a0dc42b10a1a08b61a9064e62ebe467aa0
Darshan <darshanhelode01@gmail.com>
2023-04-06 14:33:21 +0530

    first commit

---
+++ demo/demo/src/main/java/com/MyJavaTechie/demo/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.MyJavaTechie.demo.controller;
+
+import com.MyJavaTechie.demo.entity.Product;
+import com.MyJavaTechie.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @Autowired
+private ProductService productService;
+    @PostMapping("/addProduct")
+
+    public Product addProduct(@RequestBody Product product)
+    {
+    return productService.saveProduct(product);
+    }
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products)
+    {
+        return productService.saveProduct(products);
+    }
+    @GetMapping("/Products")
+    public List<Product> findAllProducts()
+    {
+        return productService.getProducts();
+    }
+    @GetMapping("/Product/{id}")
+    public Product findProductById(@PathVariable int id)
+    {
+        return productService.getProductById(id);
+    }
+    @GetMapping("/ProductByName/{name}")
+    public Product findProductByName(@PathVariable String name)
+    {
+        return productService.getProductByName(name);
+    }
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product)
+    {
+       return productService.updateProduct(product);
+    }
+    @DeleteMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable int id)
+    {
+        return productService.deleteProduct(id);
+    }
+
+}
---
+++ demo/demo/src/main/java/com/MyJavaTechie/demo/entity/Product.java
@@ -0,0 +1,21 @@
+package com.MyJavaTechie.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Product_Tb")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+}
